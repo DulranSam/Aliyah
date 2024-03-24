9a1be9f6636896cba703f53f986d3f0d05520bb5
Rukshan Dias <diasrukshan21@gmail.com>
2024-03-24 15:49:27 +0530

    display the feedback

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -68,6 +68,8 @@ function App() {
   const [listofStatProb,setListOfStatProb] = useState({});
   const [listofpureTopics,setListofPureTopics] = useState([]);
   const [listofStatTopics,setListStatTopics] = useState([]);
+  const [shortenPureMaths,setshortenPureMaths] = useState([]);
+  const [shortenstats,setshortenstats] = useState([]);
 
   const BASE = "https://aliyah-dlyb.onrender.com";
 
@@ -155,7 +157,11 @@ function App() {
     listofpureTopics,
     setListofPureTopics,
     listofStatTopics,
-    setListStatTopics
+    setListStatTopics,
+    shortenPureMaths,
+    setshortenPureMaths,
+    shortenstats,
+    setshortenstats
   };
 
   return (
