ddccccae75c32866d50fc5010f5b15cdd3f433ad
Radhul Silva <radhulsilva@gmail.com>
2024-03-28 08:36:09 +0530

    fixed ci-cd

--- backend/__test__/integration/getTopics.test.js
+++ backend/__test__/integration/getTopics.test.js
@@ -1,5 +1,5 @@
 const request = require("supertest");
-const BASE = "http://localhost:8000";
+const BASE = "https://aliyah-dlyb.onrender.com";
 
 describe("POST getTopics", () => {
   it("should return 400 if the topic source is missing", async () => {
